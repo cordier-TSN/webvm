e99f6e83b7f1dfd69f42c8863652773c878e2835
cordier <cordier@n14c4wh64>
2024-11-24 10:00:06 +0100

    bionic test

--- examples/python3/prepare_env.sh
+++ examples/python3/prepare_env.sh
@@ -1,17 +1,18 @@
-alias ll='ls -l --color=never'
-alias la='ls -la --color=never'
-cd /home/root/Workspace/python3/robotframework-7.1.1/
+alias ll = 'ls -l --color=never'
+alias la = 'ls -la --color=never'
+cd /home/user/Workspace/python3/robotframework-7.1.1/
 python3 setup.py build
 python3 setup.py install
-cd /home/root/repo
+cd /home/user/repo
 git init --bare robot.git
-cd /home/root/Workspace/robot/
+cd /home/user/Workspace/robot/
 git init
-git config --global --add safe.directory /home/root/Workspace/robot
+git config --global --add safe.directory /home/user/Workspace/robot
 git add tests
+git add libraries
 git add resources
-git config --global user.email "root"
-git config --global user.name "root"
+git config --global user.email "user"
+git config --global user.name "user"
 git commit -m "initialisation depot"
-git remote add origin /home/root/repo/robot.git
+git remote add origin /home/user/repo/robot.git
 git push --set-upstream origin master
