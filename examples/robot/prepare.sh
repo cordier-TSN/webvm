ed8ce642b768863f067e7a57d931058c68e31df5
cordier-TSN <Cordier.claude.christian@gmail.com>
2025-06-02 09:22:38 +0200

    Nouvelle Version

--- examples/robot/prepare.sh
+++ examples/robot/prepare.sh
@@ -1,6 +1,7 @@
-alias ll="ls -l"
-alias la="ls -la"
+alias ll='ls -l --color=never'
+alias la='ls -la --color=never'
+export PYTHONPATH="/home/user/Workspace/robot/:/home/user/Workspace/robot/libraries/:/home/user/Workspace/robot/resources/"
 python3 --version
 robot --version
 vi --version
-
+clear
