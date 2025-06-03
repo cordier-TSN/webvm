9985dd335afefd371c04fa3e356dc7ee77cef617
cordier <cordier@n14c4wh64>
2025-06-03 21:38:00 +0200

    alpine

--- examples/robot/libraries/myLibFile.py
+++ examples/robot/libraries/myLibFile.py
@@ -3,16 +3,26 @@ from robot.api.deco import keyword, library
 
 @library
 class myLibFile:
+    """Librairie de demonstration personnalisée.
+    ''myLibFile'' n'est pas une librairie standard 
+    de Robot Framework
+    Elle a été crée uniquement dans un but
+    de demonstration d'integration python
+    """
     def __init__(self):
         self.f = 'demofile.txt'
 
     @keyword
     def read(self):
+        """renvoi le contenu du fichier demofile.txt
+        """
         with open(self.f,'r') as file:
             return file.readline()
 
     @keyword
     def write(self, value):
+        """ecrit dans le fichier demofile.txt
+        """
         with open(self.f,'w') as file:
             file.write(str(value))
 
--- examples/robot/prepare.sh
+++ examples/robot/prepare.sh
@@ -1,7 +1,2 @@
-alias ll='ls -l --color=never'
-alias la='ls -la --color=never'
 export PYTHONPATH="/home/user/Workspace/robot/:/home/user/Workspace/robot/libraries/:/home/user/Workspace/robot/resources/"
-python3 --version
-robot --version
-vi --version
-clear
+vim -O /home/user/Workspace/robot/libraries/myLibFile.py /home/user/Workspace/robot/resources/common.resource /home/user/Workspace/robot/tests/suite.robot -c :tabnew -c :term
